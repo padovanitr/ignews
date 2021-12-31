d95f987fc52b3a596c2fba07b2525b6716553466
Thiago Padovani <padovanitr@gmail.com>
2021-12-30 22:02:08 -0300

    header structure and style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import styles from '../styles/home.module.scss'
 import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ignews</title>
+        <title>ig news 2</title>
       </Head>
-      <h1 className={styles.title}>
+      <h1>
+        hello world
       </h1>
     </>
   )
