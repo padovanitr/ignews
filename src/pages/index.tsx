a982763f1be075487479d05e1270cfe3cd07a681
Thiago Padovani <padovanitr@gmail.com>
2021-12-30 22:40:14 -0300

    implement home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig news 2</title>
+        <title>Home | ig.news</title>
       </Head>
       <h1>
-        hello world
+        <main className={styles.contentContainer}>
+          <section className={styles.hero}>
+            <span>Hey, welcome</span>
+            <h1>News about the <span>React</span> world.</h1>
+            <p>
+              Get access to all the publications <br />
+              <span>for $9.90 month</span>
+            </p>
+          </section>
+          <img src="/images/avatar.svg" alt="Girl coding" />
+        </main>
       </h1>
     </>
   )
