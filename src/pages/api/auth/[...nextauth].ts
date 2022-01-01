078a899dd9af6fbb0efc63d38f6bc62433a7bc98
Thiago Padovani <padovanitr@gmail.com>
2022-01-01 17:38:38 -0300

    setup signin process

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb' 
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -13,4 +17,24 @@ export default NextAuth({
       }
     }),
   ],
+  secret: process.env.SIGNINKEY,
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user;
+      
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            {data: {email} }
+          )
+        )
+
+        return true
+      } catch (error) {
+        return false
+      }
+
+    },
+  }
 })
