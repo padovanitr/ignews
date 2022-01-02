862d24fa54cf7c44df8a2ceaa89ec658f3a2af91
Thiago Padovani <padovanitr@gmail.com>
2022-01-02 00:05:07 -0300

    stripe create subscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,32 @@
+import { useSession, signIn } from 'next-auth/react';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
+// places in next where you can variable environments and make secure procedures
+// getServerSideProps (SSR) - useful for on loading pages
+// getStaticProps (SSG)     - useful for on loading pages
+// API routes               - useful for user's actions
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps){
+    const { data: session, status } = useSession()
+
+    function handleSubscribe() {
+        if (!session) {
+            signIn('github')
+            return;
+        }
+
+        
+    }
+
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
