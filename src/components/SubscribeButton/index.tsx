9a1e2daf097d16f68cead96d3e45c34b7956c3cf
Thiago Padovani <padovanitr@gmail.com>
2022-01-03 07:54:15 -0300

    handle subscription by clicking on button and redirect to stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -13,13 +15,23 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps){
     const { data: session, status } = useSession()
 
-    function handleSubscribe() {
+    async function handleSubscribe() {
         if (!session) {
             signIn('github')
             return;
         }
 
-        
+        try {
+          const response = await api.post('/subscribe')
+
+          const { sessionId } = response.data;
+          
+          const stripe = await getStripeJs()
+
+          await stripe.redirectToCheckout({ sessionId })
+        } catch (error) {
+            alert(error.message)
+        }
     }
 
     return (
