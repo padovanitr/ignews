1e2c544ccf4bb1aafb63946827213ba36bc84780
Thiago Padovani <padovanitr@gmail.com>
2022-01-07 15:34:07 -0300

    listenning to other events from stripe like delete and update

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -6,6 +6,7 @@ import { stripe } from '../../../services/stripe';
 export async function saveSubscription(
     subscriptionId: string, 
     customerId: string,
+    createAction = false
 ) {
     // 1º - buscar o usuario no banco do fauna com Id(customerId) que é o stripe_customer_id
     const userRef = await fauna.query(
@@ -30,10 +31,27 @@ export async function saveSubscription(
     }
 
     // 2º salvar os dados da subscription no faunadb
-    await fauna.query(
-        q.Create(
-            q.Collection('subscriptions'),
-            { data: subscriptionData }
+    if (createAction) {
+        await fauna.query(
+            q.Create(
+                q.Collection('subscriptions'),
+                { data: subscriptionData }
+            )
         )
-    )
+    } else {
+        await fauna.query(
+            q.Replace(
+                q.Select(
+                    "ref",
+                    q.Get(
+                        q.Match(
+                            q.Index('subscription_by_id'),
+                            subscriptionId
+                        )
+                    )
+                ),
+                { data: subscriptionData }
+            )
+        )
+    }
 }
