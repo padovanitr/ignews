0e1151fd39154dbb869b7221c6e5c4d1f47b3c96
Thiago Padovani <padovanitr@gmail.com>
2022-01-08 14:49:05 -0300

    consuming prismic api to get posts data

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,8 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
+import Prismic from '@prismicio/client';
 
 export default function Posts() {
     return (
@@ -10,19 +13,19 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a>
+                    <a href="#">
                         <time>12 de março de 2021</time>
                         <strong>Createing a monorepo with lerna</strong>
                         <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
                     </a>
 
-                    <a>
+                    <a href="#">
                         <time>12 de março de 2021</time>
                         <strong>Createing a monorepo with lerna</strong>
                         <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
                     </a>
 
-                    <a>
+                    <a href="#">
                         <time>12 de março de 2021</time>
                         <strong>Createing a monorepo with lerna</strong>
                         <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
@@ -31,4 +34,21 @@ export default function Posts() {
             </main>
         </>
     )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'publication')
+    ], { 
+        fetch: ['publication.title', 'publication.content'],
+        pageSize: 100,
+    })
+
+    console.log(JSON.stringify(response, null, 2));
+
+    return {
+        props: {}
+    }
 }
