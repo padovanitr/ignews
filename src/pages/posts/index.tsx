fc7044d631b56084ac783e662efc93640608550a
Thiago Padovani <padovanitr@gmail.com>
2022-01-08 14:07:21 -0300

    style posts page

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Posts() {
+    return (
+        <>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
+
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    <a>
+                        <time>12 de março de 2021</time>
+                        <strong>Createing a monorepo with lerna</strong>
+                        <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                    </a>
+
+                    <a>
+                        <time>12 de março de 2021</time>
+                        <strong>Createing a monorepo with lerna</strong>
+                        <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                    </a>
+
+                    <a>
+                        <time>12 de março de 2021</time>
+                        <strong>Createing a monorepo with lerna</strong>
+                        <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                    </a>
+                </div>
+            </main>
+        </>
+    )
+}
